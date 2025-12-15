34e13751ce3968148220f98feb0c0956458e3856
Markus <markusseiler@gmx.net>
2025-12-15 09:07:58 +0100

    update for working webpage

--- app.py
+++ app.py
@@ -304,28 +304,29 @@ elif st.session_state.page == 'main':
                 b64_svg = base64.b64encode(icon_svg.encode('utf-8')).decode("utf-8")
 
                 with c_cols[idx]:
-                    st.markdown(f"""
-                    <div style="
-                        background-color: #262730; 
-                        border: 1px solid #444; 
-                        border-bottom: 3px solid {score_color}; 
-                        border-radius: 8px; 
-                        padding: 10px; 
-                        text-align: center; 
-                        height: 200px;
-                        display: flex; flex-direction: column; justify-content: space-between; align-items: center;">
+                    # We use textwrap.dedent to remove the indentation so Streamlit renders it as HTML, not code.
+                    html_code = textwrap.dedent(f"""
+                                    <div style="
+                                        background-color: #262730; 
+                                        border: 1px solid #444; 
+                                        border-bottom: 3px solid {score_color}; 
+                                        border-radius: 8px; 
+                                        padding: 10px; 
+                                        text-align: center; 
+                                        height: 200px;
+                                        display: flex; flex-direction: column; justify-content: space-between; align-items: center;">
 
-                        <img src="data:image/svg+xml;base64,{b64_svg}" style="width:50px; height:50px; opacity:0.9;">
+                                        <img src="data:image/svg+xml;base64,{b64_svg}" style="width:50px; height:50px; opacity:0.9;">
 
-                        <div style="margin-top:5px;">
-                            <div style="font-weight:bold; color: #fff; font-size:1.1em;">{t_data['name']}</div>
-                            <div style="font-size:0.8em; color: {color};">{t_data['type']} • {t_data['role']}</div>
-                        </div>
+                                        <div style="margin-top:5px;">
+                                            <div style="font-weight:bold; color: #fff; font-size:1.1em;">{t_data['name']}</div>
+                                            <div style="font-size:0.8em; color: {color};">{t_data['type']} • {t_data['role']}</div>
+                                        </div>
 
-                        <div style="width:100%;">
-                            <div style="font-size:1.2em; font-weight:900; color:{score_color};">{score} pts</div>
-                            <div style="font-size:0.7em; color: #aaa; line-height:1.2; min-height:30px;">{item['note']}</div>
-                        </div>
-                    </div>
-                    """, unsafe_allow_html=True)
-            st.markdown("<br>", unsafe_allow_html=True)
+                                        <div style="width:100%;">
+                                            <div style="font-size:1.2em; font-weight:900; color:{score_color};">{score} pts</div>
+                                            <div style="font-size:0.7em; color: #aaa; line-height:1.2; min-height:30px;">{item['note']}</div>
+                                        </div>
+                                    </div>
+                                    """)
+                    st.markdown(html_code, unsafe_allow_html=True)
